8fc09409f75e4e930ecd2d7082c3ee1be05d7c54
macnick <nick@macexperts.gr>
2021-05-13 17:05:30 +0300

    Create redirect logic and click count

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,5 @@
 class LinksController < ApplicationController
+  
   def show
     @link = Link.find(params[:id])
   end
@@ -9,16 +10,26 @@ class LinksController < ApplicationController
   def create
     @link = Link.new(params.require(:link).permit(:url))
     @link.shortened = shorten
+    @link.clicked = 0
     @link.save
+    # render plain: @link.inspect
     redirect_to @link
+  end
+
+  def redirect
+    @link = Link.find_by(shortened: params[:shortened])#_shortened(params[:shortened]) 
+    # render plain: @link.inspect
+    render 'errors/404', status: 404 if @link.nil?
+    @link.update_attribute(:clicked, @link.clicked + 1)
+    redirect_to @link.url
   end
 
   private
 
     def shorten
-      host = 'https://localhost:3000//s/'
+      host = 'http://localhost:3000//s/'
       charset = Array('A'..'Z') + Array('a'..'z') + Array('0'..'9')
-      host + Array.new(7) { charset.sample }.join
+      Array.new(7) { charset.sample }.join
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "links#new"
   resources :links, only: [:new, :create, :show]
+  get '/s/:shortened', to: 'links#redirect', as: :shortened
 end
