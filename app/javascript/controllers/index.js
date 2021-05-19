a154dae3f129868cabb8a77548689f3ddc59e978
macnick <nick@macexperts.gr>
2021-05-19 11:58:07 +0300

    Test stimulus installation

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-const application = Application.start(document.documentElement)
-const context = require.context(("./controllers", true, /\.js$/))
+const application = Application.start()
+const context = require.context(".", true, /\.js$/)
 application.load(definitionsFromContext(context))
